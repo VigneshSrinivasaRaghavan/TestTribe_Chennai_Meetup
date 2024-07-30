33d256d6a0df463eda64120ce6dc754fefca7ba8
Vignesh Srinivasa Raghavan <40608099+VigneshSrinivasaRaghavan@users.noreply.github.com>
2024-07-30 23:44:16 +0530

    Allure environment config metadata

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
+import * as os from "os";
 
 /**
  * Read environment variables from file.
@@ -22,7 +23,16 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', {open: 'never'}],['allure-playwright']],
+  reporter: [['html', {open: 'never'}],['allure-playwright',{
+    detail: true,
+    suiteTitle: false,
+    environmentInfo: {
+      os_platform: os.platform(),
+      os_release: os.release(),
+      os_version: os.version(),
+      node_version: process.version,
+    },
+  },]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
